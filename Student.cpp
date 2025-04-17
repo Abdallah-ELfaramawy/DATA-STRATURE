e79f389c098afeaa1786c4ab513107705d8008d8
Abdallah-ELfaramawy <abdalallahwalid@gmail.com>
2025-04-17 17:08:20 +0200

    Create Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <vector>
+#include<map>
+#include "User.h"
+using namespace std;
+
+class Student:public User {
+private:
+    int id;
+    string UserName;
+    string Password;
+    vector<string> enrolledCourses;
+    map<string, double>grades;
+
+public:
+    Student();
+    Student(int id, string password, vector<string>enrolledCourses, map<string, double>grades);
+    
+    int getStudentID() const;
+    string getName() const;
+    vector<string> getEnrolledCourses() ;
+    map<string, double> getGrades() ;
+
+  
+    void setStudentID(int id);
+    void setName(const string& studentName);
+    
+
+};
